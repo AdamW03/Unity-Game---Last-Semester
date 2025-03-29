36c13a792e01349785a76a88cb13a5e4ad7b786e
Mariusz Gitara <gitaramariusz9@gmail.com>
2025-03-29 19:02:49 +0100

    resetv2
    moze zadziala

--- Assets/classes/Item.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
-public class Item
-{
-    public string itemName;
-    public Sprite itemIcon;
-    public string description;
-}
