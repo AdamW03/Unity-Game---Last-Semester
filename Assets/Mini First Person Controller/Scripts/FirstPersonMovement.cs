db34746ac2ca360af76ce4035afebf830ff2c5d8
Mariusz Gitara <gitaramariusz9@gmail.com>
2025-05-25 19:56:46 +0200

    controllerFix
    wchodzenie po schodach (do konca nie dziala), brak przyklejania sie do sciany

--- Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -1,30 +1,50 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class FirstPersonMovement : MonoBehaviour
 {
-    public float speed = 4;
+    public float speed = 3;
+
+    [Header("Player Step Climb: ")]
+    Rigidbody rigidbody;
+    [SerializeField] GameObject stepRayUpper;
+    [SerializeField] GameObject stepRayLower;
+    [SerializeField] float stepHeight = 0.3f;
+    [SerializeField] float stepSmooth = 2f;
+
 
     [Header("Running")]
     public bool canRun = true;
     public bool IsRunning { get; private set; }
-    public float runSpeed = 7;
+    public float runSpeed = 6;
     public KeyCode runningKey = KeyCode.LeftShift;
 
-    Rigidbody rigidbody;
+    [Header("Custom Gravity")] // Dodajemy nową sekcję
+    public float gravityMultiplier = 3f; // Mnożnik grawitacji
+
     /// <summary> Functions to override movement speed. Will use the last added override. </summary>
     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
 
-
-
     void Awake()
     {
-        // Get the rigidbody on this.
         rigidbody = GetComponent<Rigidbody>();
+        if (rigidbody != null)
+        {
+            rigidbody.useGravity = false; // Wyłączamy standardową grawitację Unity
+        }
+        else
+        {
+            Debug.LogError("Rigidbody component not found on this GameObject!", this);
+        }
+        stepRayUpper.transform.position = new Vector3(stepRayUpper.transform.position.x, stepHeight, stepRayUpper.transform.position.z);
     }
 
     void FixedUpdate()
     {
+        // Zastosuj niestandardową grawitację
+        ApplyCustomGravity();
+
         // Update IsRunning from input.
         IsRunning = canRun && Input.GetKey(runningKey);
 
@@ -36,9 +56,66 @@ public class FirstPersonMovement : MonoBehaviour
         }
 
         // Get targetVelocity from input.
-        Vector2 targetVelocity =new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+        Vector2 targetVelocity = new Vector2(Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
 
         // Apply movement.
-        rigidbody.linearVelocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.linearVelocity.y, targetVelocity.y);
+        if (rigidbody != null)
+        {
+            // Zachowujemy obecną prędkość pionową (na którą wpływa nasza niestandardowa grawitacja)
+            // i ustawiamy prędkości poziome na podstawie inputu.
+            Vector3 currentHorizontalVelocity = transform.rotation * new Vector3(targetVelocity.x, 0, targetVelocity.y);
+            rigidbody.linearVelocity = new Vector3(currentHorizontalVelocity.x, rigidbody.linearVelocity.y, currentHorizontalVelocity.z);
+        }
+
+
+        stepClimb();
+    }
+
+    void ApplyCustomGravity()
+    {
+        if (rigidbody != null)
+        {
+            // Physics.gravity to standardowy wektor grawitacji w Unity (zazwyczaj (0, -9.81, 0))
+            // Mnożymy go przez nasz modyfikator
+            Vector3 customGravity = Physics.gravity * gravityMultiplier;
+            rigidbody.AddForce(customGravity, ForceMode.Acceleration);
+            // ForceMode.Acceleration sprawia, że siła działa niezależnie od masy obiektu,
+            // tak jak prawdziwa grawitacja.
+        }
+    }
+
+    void stepClimb()
+    {
+        RaycastHit hitLower;
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward), out hitLower, 0.1f))
+        {
+            RaycastHit hitUpper;
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(Vector3.forward), out hitUpper, 0.2f))
+            {
+                rigidbody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+            }
+        }
+
+        RaycastHit hitLower45;
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitLower45, 0.1f))
+        {
+
+            RaycastHit hitUpper45;
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitUpper45, 0.2f))
+            {
+                rigidbody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+            }
+        }
+
+        RaycastHit hitLowerMinus45;
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, 0.1f))
+        {
+
+            RaycastHit hitUpperMinus45;
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitUpperMinus45, 0.2f))
+            {
+                rigidbody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+            }
+        }
     }
 }
